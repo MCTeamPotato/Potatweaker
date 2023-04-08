e8025b46a9953234f04c6821bd5767910b52a7b5
Kasualix <670067575@qq.com>
2023-04-08 12:05:40 +0800

    close #1

--- src/main/java/com/teampotato/potatweaker/EventHandler.java
+++ src/main/java/com/teampotato/potatweaker/EventHandler.java
@@ -5,6 +5,8 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -13,16 +15,13 @@ import java.util.Objects;
 @Mod.EventBusSubscriber
 public class EventHandler {
     @SubscribeEvent
-    public static void ctrlSpawn(EntityJoinWorldEvent event) {
-        World world = event.getWorld();
-        if (world.isClientSide || Config.REPLACED.get().isEmpty()) return;
-
+    public static void ctrlSpawn(LivingSpawnEvent.CheckSpawn event) {
         Entity replaced = event.getEntity();
-        String name = Objects.requireNonNull(replaced.getType().getRegistryName()).toString();
+        World world = replaced.level;
+        if (world.isClientSide || Config.REPLACED.get().isEmpty() || replaced.getType().getRegistryName() == null) return;
 
-        int index = Config.REPLACED.get().indexOf(name);
+        int index = Config.REPLACED.get().indexOf(replaced.getType().getRegistryName().toString());
         if (index == -1) return;
-        if (replaced.getTags().contains("potatweaker.spawned")) return;
 
         boolean removalMatchesChance = Config.REMOVAL_MATCHES_CHANCE.get().get(index);
         boolean isRemoval = Config.REMOVE.get().get(index);
@@ -30,17 +29,16 @@ public class EventHandler {
 
         if (isRemoval) {
             if (removalMatchesChance) {
-                if (replaceChance > world.getRandom().nextInt(101)) event.setCanceled(true);
+                if (replaceChance > world.getRandom().nextInt(101)) event.setResult(Event.Result.DENY);
             } else {
-                event.setCanceled(true);
+                event.setResult(Event.Result.DENY);
             }
         } else {
             if (replaceChance > world.getRandom().nextInt(101)) {
-                event.setCanceled(true);
+                event.setResult(Event.Result.DENY);
                 summonHelper(Objects.requireNonNull(world.getServer()), index, replaced);
             }
         }
-        if(!event.isCanceled()) replaced.addTag("potatweaker.spawned");
     }
 
     private static void summonHelper(MinecraftServer server, Integer index, Entity replaced) {
