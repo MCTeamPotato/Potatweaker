b7fc5fc93fed093c26282a44ce0d16c334262075
Kasualix <670067575@qq.com>
2023-04-10 02:07:35 +0800

    branch

--- src/main/java/com/teampotato/potatweaker/EventHandler.java
+++ src/main/java/com/teampotato/potatweaker/EventHandler.java
@@ -4,8 +4,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent;
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.MobSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,12 +13,13 @@ import java.util.Objects;
 @Mod.EventBusSubscriber
 public class EventHandler {
     @SubscribeEvent
-    public static void ctrlSpawn(LivingSpawnEvent.CheckSpawn event) {
+    public static void ctrlSpawn(MobSpawnEvent.FinalizeSpawn event) {
         Entity replaced = event.getEntity();
         Level world = replaced.level;
-        if (world.isClientSide || Config.REPLACED.get().isEmpty() || replaced.getType().getRegistryName() == null) return;
+        if (world.isClientSide || Config.REPLACED.get().isEmpty()) return;
+        String type = replaced.getType().toString();
 
-        int index = Config.REPLACED.get().indexOf(replaced.getType().getRegistryName().toString());
+        int index = Config.REPLACED.get().indexOf(type.split("\\.")[1] + ":" + type.split("\\.")[2]);
         if (index == -1) return;
 
         boolean removalMatchesChance = Config.REMOVAL_MATCHES_CHANCE.get().get(index);
@@ -28,13 +28,13 @@ public class EventHandler {
 
         if (isRemoval) {
             if (removalMatchesChance) {
-                if (replaceChance > world.getRandom().nextInt(101)) event.setResult(Event.Result.DENY);
+                if (replaceChance > world.getRandom().nextInt(101)) event.setSpawnCancelled(true);
             } else {
-                event.setResult(Event.Result.DENY);
+                event.setSpawnCancelled(true);
             }
         } else {
             if (replaceChance > world.getRandom().nextInt(101)) {
-                event.setResult(Event.Result.DENY);
+                event.setSpawnCancelled(true);
                 summonHelper(Objects.requireNonNull(world.getServer()), index, replaced);
             }
         }
@@ -49,6 +49,6 @@ public class EventHandler {
 
         String cmd = "/execute in " + dim + " run summon " + replacer + " " + pos.x + " " + pos.y + " " + pos.z + " {" + nbt + "}";
 
-        server.getCommands().performCommand(server.createCommandSourceStack().withSuppressedOutput(), cmd);
+        server.getCommands().performPrefixedCommand(server.createCommandSourceStack().withSuppressedOutput(), cmd);
     }
 }
