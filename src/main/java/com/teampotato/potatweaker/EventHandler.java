891eabb3860bb1f683236ebed3555636e943e550
Kasualix <670067575@qq.com>
2023-09-19 13:18:48 +0800

    1182-1.1.2

--- src/main/java/com/teampotato/potatweaker/EventHandler.java
+++ src/main/java/com/teampotato/potatweaker/EventHandler.java
@@ -5,7 +5,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,12 +18,12 @@ public class EventHandler {
     private static final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     @SubscribeEvent
-    public static void ctrlSpawn(@NotNull EntityJoinLevelEvent event) {
+    public static void ctrlSpawn(@NotNull EntityJoinWorldEvent event) {
         Entity replaced = event.getEntity();
         Level world = replaced.getCommandSenderWorld();
         MinecraftServer server = world.getServer();
         if (world.isClientSide || Config.REPLACED.get().isEmpty() || server == null || replaced.getTags().contains("potatweaker.spawned")) return;
-        ResourceLocation type = ForgeRegistries.ENTITY_TYPES.getKey(replaced.getType());
+        ResourceLocation type = replaced.getType().getRegistryName();
         if (type == null) return;
 
         int index = Config.REPLACED.get().indexOf(type.toString());
@@ -63,6 +63,6 @@ public class EventHandler {
             cmd = "/execute in " + dim + " run summon " + replacer + " " + pos.x + " " + pos.y + " " + pos.z;
         }
 
-        server.getCommands().performPrefixedCommand(server.createCommandSourceStack().withSuppressedOutput(), cmd);
+        server.getCommands().performCommand(server.createCommandSourceStack().withSuppressedOutput(), cmd);
     }
 }
